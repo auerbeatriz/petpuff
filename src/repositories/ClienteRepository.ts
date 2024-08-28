692930364ba57cb90ad579f910c3faf637027287
Pietro <pedrohtbernabe@gmail.com>
2024-08-28 03:20:02 -0300

    Cliente repository

--- src/repositories/ClienteRepository.ts
+++ src/repositories/ClienteRepository.ts
@@ -1,20 +1,63 @@
-import { Repository } from 'typeorm';
-import { DataBaseConnection } from '../config/typeorm';
-import { Cliente } from '../entities/Cliente';
-import { Login } from '../entities/Login';
+import { DataBaseConnection } from "../config/typeorm";
+import { Cliente } from "../entities/Cliente";
+import { CheckoutCliente } from "../types/Checkout.interface";
+import { BadRequestError } from "../types/erros/BadRequestError";
+import { NotFoundError } from "../types/erros/NotFoundError";
+import { EnderecoRepository } from "./EnderecoRepository";
 
-export class ClienteRepository extends Repository<Cliente> {
-    static getCliente(clienteId: number) {
-        throw new Error("Method not implemented.");
+export class ClienteRepository {
+    static repository = DataBaseConnection.getRepository(Cliente)
+
+    static async get(clienteId: number) {
+        return (await this.repository.find({ where: { id: clienteId }}))[0]
     }
-    
+
+    static async getByLogin(login: string): Promise<Cliente> {
+        const cliente = await this.repository.findOne({
+            select: {
+                id: true,
+                nome: true,
+                sobrenome: true,
+                cpf: true,
+                email: true,
+                celular: true
+            },
+            relations: {
+                endereco: true
+            },
+            where: {
+                login: { username: login }
+            }
+        })
+
+        if(!cliente) {
+            throw new NotFoundError(`Nenhum cliente com login: {${ login }}`)
+        }
+
+        return cliente
+    }
+
+    static async atualizarEndereco(cliente: CheckoutCliente) {
+        const { id, enderecoFiscal, cpf } = cliente
+        const clienteBD = await this.get(id)
+
+        if(clienteBD.cpf === cpf) {
+            if(!enderecoFiscal) {
+                throw new BadRequestError('Nenhum endereço fiscal encontrado.')
+            }
+
+            const endereco = await EnderecoRepository.criar(enderecoFiscal)
+            await this.repository.update(id, {
+                endereco
+            })
+        }
+    }
+
+
     static async findById(id: number): Promise<Cliente | null> {
         const clienteRepository = DataBaseConnection.getRepository(Cliente);
         const cliente = await clienteRepository.findOne({ where: { id } });
         return cliente || null;
-    }
-    constructor() {
-        super(Cliente, DataBaseConnection.manager);
     }
 
     static async createCliente(email: string, nome: string, sobrenome: string, cpf: string, celular: string, login: Cliente['login']): Promise<Cliente> {
